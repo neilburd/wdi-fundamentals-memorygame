963036b5ccc4ec300e19e816446573d4af0d86ed
“neilburd” <neilburd@gmail.com>
2016-11-10 16:18:50 -0500

    Initial JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,11 @@
-console.log("JS file is connected to HTML! Woo!")
+//console.log("JS file is connected to HTML! Woo!")
+var cardOne = 'king';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'queen';
+
+//  **** Checking my Variables ***
+//console.log('cardOne =' + ' ' + cardOne)
+//console.log('cardTwo =' + ' ' + cardTwo)
+//console.log('cardThree =' + ' ' + cardThree)
+//console.log('cardFour =' + ' ' + cardFour)
