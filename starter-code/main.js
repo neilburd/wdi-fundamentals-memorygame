54e5d336001b8a2d4b37559c6419f26f7ffb0ba0
“neilburd” <neilburd@gmail.com>
2016-11-12 12:56:13 -0500

    added create card function

--- starter-code/main.js
+++ starter-code/main.js
@@ -12,5 +12,23 @@ if (cardTwo === cardThree) {
   alert('You Found A Match!');
 }
 else {
-  alert('Sorry Try Again.')
+  alert('Sorry Try Again.');
 }
+
+
+var createCards = function(numberOfCards){
+  // adding cards through JS
+  var cards = document.getElementById('game-board');
+
+  //var numberOfCards = 4;
+  // go through number of cards
+  for (var i = 0; i < numberOfCards; i++) {
+    //create div
+    var newDivForCard = document.createElement('div');
+    //update div with card class
+    newDivForCard.setAttribute('class', 'card');
+    //append cards to add newDivForCard
+    cards.appendChild(newDivForCard);
+  }
+}
+createCards(4);
